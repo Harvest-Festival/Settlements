55479f63899b09a63a9a082ce9d824ca309e0d05
joshiejack <joshjackwildman@gmail.com>
2024-03-14 22:13:43 +0000

    Disable test quests

--- src/main/java/uk/joshiejack/settlements/data/SettlementsQuests.java
+++ src/main/java/uk/joshiejack/settlements/data/SettlementsQuests.java
@@ -5,7 +5,6 @@ import net.minecraft.resources.ResourceLocation;
 import uk.joshiejack.penguinlib.data.generator.AbstractPenguinRegistryProvider;
 import uk.joshiejack.penguinlib.util.registry.ReloadableRegistry;
 import uk.joshiejack.settlements.world.quest.Quest;
-import uk.joshiejack.settlements.world.quest.settings.Settings;
 
 import java.util.Map;
 
@@ -16,8 +15,8 @@ public class SettlementsQuests extends AbstractPenguinRegistryProvider<Quest> {
 
     @Override
     protected void buildRegistry(Map<ResourceLocation, Quest> map) {
-        map.put(new ResourceLocation("settlements", "test"), new Quest(Settings.DEFAULT));
-        map.put(new ResourceLocation("settlements", "test/test2"), new Quest(Settings.DEFAULT));
-        map.put(new ResourceLocation("settlements", "test/test3"), new Quest(Settings.DEFAULT));
+        //map.put(new ResourceLocation("settlements", "test"), new Quest(Settings.DEFAULT));
+        //map.put(new ResourceLocation("settlements", "test/test2"), new Quest(Settings.DEFAULT));
+        //map.put(new ResourceLocation("settlements", "test/test3"), new Quest(Settings.DEFAULT));
     }
 }
